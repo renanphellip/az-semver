89bdef5edef942f2487ebd350443437c12ffadd0
Renan Morais <renan.morais@dxc.com>
2022-02-03 18:58:10 -0300

    Updated authentication mode to azGetApi.ts

--- src/extensions/azGetApi.ts
+++ src/extensions/azGetApi.ts
@@ -1,8 +1,8 @@
 import { GluegunToolbox } from "gluegun";
-import { WebApi, getPersonalAccessTokenHandler } from "azure-devops-node-api";
+import { WebApi, getBearerHandler } from "azure-devops-node-api";
 
 module.exports = async (toolbox: GluegunToolbox) => {
   toolbox.azGetApi = async (azOrgUrl: string, azAccessToken: string): Promise<WebApi> => {
-    return new WebApi(azOrgUrl, getPersonalAccessTokenHandler(azAccessToken), { ignoreSslError: true });
+    return new WebApi(azOrgUrl, getBearerHandler(azAccessToken), { ignoreSslError: true });
   }
 }
